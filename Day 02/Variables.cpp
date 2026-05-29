4a001744d58291e1cbd8b39697330960067bd257
Gulshan Rahangdale <gulshanrahangdale46@gmail.com>
2026-05-29 23:41:09 +0530

    Update Variables.cpp

--- Day 02/Variables.cpp
+++ Day 02/Variables.cpp
@@ -14,7 +14,7 @@ int main() {
     // char is a data type that can store a single character. It typically uses 1 byte of memory and can store values from -128 to 127 (or 0 to 255 if unsigned).
     string name = "Gulshan";
     // string is a data type that can store a sequence of characters (text). It is not a built-in data type in C++, but it is provided by the C++ Standard Library. A string can store any combination of letters, numbers, and symbols, and it can be of variable length.
-    bool student = true;
+    bool student = true; 
     // bool is a data type that can store one of two values: true or false. It typically uses 1 byte of memory and is used to represent logical values.
 
     cout << age << endl;
