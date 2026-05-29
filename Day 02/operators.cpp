44bc19e7b3ae162fba88e61512b8351fae27beb9
Gulshan1304 <gulshanrahangdale45@gmail.com>
2026-05-29 11:36:16 -0700

    Complete Operators

--- Day 02/operators.cpp
+++ Day 02/operators.cpp
@@ -1,3 +1,24 @@
 #include<iostream>
 using namespace std;
-int main() {}
+int main() {
+
+    int a = 10;
+    int b = 20;
+
+    // Arithmetic Operators
+    cout << "a + b = " << a + b << endl; // Addition
+    cout << "a - b = " << a - b << endl; // Subtraction
+    cout << "a * b = " << a * b << endl; // Multiplication
+    cout << "b / a = " << b / a << endl; // Division
+    cout << "b % a = " << b % a << endl; // Modulus
+
+    // Comparison Operators
+    cout << "a == b: " << (a == b) << endl; // Equal to
+    cout << "a != b: " << (a != b) << endl; // Not equal to
+    cout << "a > b: " << (a > b) << endl;   // Greater than
+    cout << "a < b: " << (a < b) << endl;   // Less than
+    cout << "a >= b: " << (a >= b) << endl; // Greater than or equal to
+    cout << "a <= b: " << (a <= b) << endl; // Less than or equal to
+
+    return 0;
+}
