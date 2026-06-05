9f8d810fba662dd868aa57670ef5b23ecd00a6ec
gulshan1304 <gulshanrahangdale46@gmail.com>
2026-06-05 13:26:07 -0700

    loop completed

--- Day 05/loop.cpp
+++ Day 05/loop.cpp
@@ -1,21 +1,89 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-// for loop
-int main () {
-    for (int i = 0; i < 5; i++) {
-        cout << i << endl ;
+
+int main()
+{
+    // FOR LOOP
+    cout << "For Loop:" << endl;
+    for (int i = 1; i <= 5; i++)
+    {
+        cout << i << endl;
     }
-    // while loop
-    int j = 0;
-    while (j < 5) {
-        cout << j << endl ;
+
+    cout << endl;
+
+    // WHILE LOOP
+    cout << "While Loop:" << endl;
+    int j = 1;
+
+    while (j <= 5)
+    {
+        cout << j << endl;
         j++;
     }
-    // do while loop
-    int k = 0;
-    do {
-        cout << k << endl ;
+
+    cout << endl;
+
+    // DO-WHILE LOOP
+    cout << "Do-While Loop:" << endl;
+    int k = 1;
+
+    do
+    {
+        cout << k << endl;
         k++;
-    } while (k < 5);
-return 0;
-}   
+    } while (k <= 5);
+
+    cout << endl;
+
+    // INFINITE LOOP (COMMENTED)
+    /*
+    while(true)
+    {
+        cout << "Hello" << endl;
+    }
+    */
+
+    // BREAK STATEMENT
+    cout << "Break Statement:" << endl;
+
+    for (int i = 1; i <= 10; i++)
+    {
+        if (i == 5)
+        {
+            break;
+        }
+
+        cout << i << endl;
+    }
+
+    cout << endl;
+
+    // CONTINUE STATEMENT
+    cout << "Continue Statement:" << endl;
+
+    for (int i = 1; i <= 10; i++)
+    {
+        if (i == 5)
+        {
+            continue;
+        }
+
+        cout << i << endl;
+    }
+
+    cout << endl;
+
+    // NESTED LOOP
+    cout << "Nested Loop:" << endl;
+
+    for (int i = 1; i <= 3; i++)
+    {
+        for (int j = 1; j <= 3; j++)
+        {
+            cout << i << " " << j << endl;
+        }
+    }
+
+    return 0;
+}
