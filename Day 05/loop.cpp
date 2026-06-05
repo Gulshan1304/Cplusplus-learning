ec1d1b0916b5f945614a77a723148a76667d9517
gulshan1304 <gulshanrahangdale46@gmail.com>
2026-06-05 11:56:46 -0700

    loop update

---
+++ Day 05/loop.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+// for loop
+int main () {
+    for (int i = 0; i < 5; i++) {
+        cout << i << endl ;
+    }
+    // while loop
+    int j = 0;
+    while (j < 5) {
+        cout << j << endl ;
+        j++;
+    }
+    // do while loop
+    int k = 0;
+    do {
+        cout << k << endl ;
+        k++;
+    } while (k < 5);
+return 0;
+}   
