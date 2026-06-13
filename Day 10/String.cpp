fd2b27c2acde5d61f8fad35a6271442a0c05898d
gulshan1304 <gulshanrahangdale46@gmail.com>
2026-06-14 05:19:45 +0530

    String

--- Day 10/String.cpp
+++ Day 10/String.cpp
@@ -1,2 +1,9 @@
 #include<iostream>
-using 
+using namespace std;
+int main () {
+    string name = "Gulshan";
+
+    cout << "My name is " << name << endl;
+    return 0;
+
+}
---
+++ Day 10/notes.cpp
@@ -0,0 +1,2 @@
+# String 
+A string is a sequence of characters used to store text such as names, sentences, orr words.
