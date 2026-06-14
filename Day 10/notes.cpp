2f27f3e675c74a0e68315a27b4bf4997ae49c989
gulshan1304 <gulshanrahangdale46@gmail.com>
2026-06-14 05:30:10 +0530

    String

--- Day 10/notes.cpp
+++ Day 10/notes.cpp
@@ -1,2 +1,17 @@
 # String 
+
 A string is a sequence of characters used to store text such as names, sentences, orr words.
+
+## Types of string
+1. C-Style String (char[])
+2. std::string (String Class)
+3. String Literal
+
+1. C-Style String (char[])
+A C-style string is an array of characters that ends with a special character called the null character ('\0').
+
+2. std::string (String Class)
+std::string is a class provided by the C++ Standard Library.
+
+3. String Literal
+A string literal is a sequence of characters enclosed in double quotes.
