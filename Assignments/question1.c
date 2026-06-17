11aea8b0d89037776c821b4d0f777288cec02062
gulshan1304 <gulshanrahangdale46@gmail.com>
2026-06-18 03:31:41 +0530

    Vowel print

---
+++ Assignments/question1.c
@@ -0,0 +1,18 @@
+// WAP to print vowel.
+
+#include <stdio.h>
+int main(){
+char ch;
+printf("Enter a Character:");
+scanf("%c ", &ch);
+
+if (ch == 'a' || ch == 'e' ||ch == 'i' ||ch == 'o' ||ch == 'u'||
+ ch == 'A' || ch == 'E' || ch == 'I' ||
+        ch == 'O' || ch == 'U'){
+printf("%c is vowel",ch);
+}
+else{
+printf("%c is consonant",ch);
+}
+return 0;
+}
