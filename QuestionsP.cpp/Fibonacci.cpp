07642931d50c10ae4fa8fde400240ba5b7947a25
gulshan1304 <gulshanrahangdale46@gmail.com>
2026-06-21 18:14:05 +0530

    Fibonacci series

--- QuestionsP.cpp/Fibonacci.cpp
+++ QuestionsP.cpp/Fibonacci.cpp
@@ -1,7 +1,18 @@
 // Recursive Fibonacci
 #include<iostream>
-using namespace st;
+using namespace std;
 void fibonacci(int r){
     int a = -1, b= 1, c;
-    
+    for (int i = 0; i<r ; i++){
+        c=a+b;
+    cout << c << " ";
+    a=b;
+    b=c;
+
+    }
+}
+int main () {
+    int r ;
+    fibonacci (r);
+    return 0;
 }
